b3215749292cf358980d276e6231f3bd849b8ee8
holyaustin <holyaustin@yahoo.com>
2023-03-27 14:37:59 +0100

    api fix commit

--- pages/symptom.js
+++ pages/symptom.js
@@ -15,7 +15,6 @@ const callGenerateEndpoint = async () => {
   
   console.log("Calling OpenAI...");
   // const response = await fetch('http://localhost:3000/api/generate', {
-  // const response = await fetch('http://localhost:3000/api/generate', {
   const response = await fetch('/api/generate', {
     method: 'POST',
     headers: {
